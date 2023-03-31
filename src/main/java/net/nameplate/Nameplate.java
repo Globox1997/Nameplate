ea4afe6a5ed437e08bfa6e22b9dd9fe87b3bd911
Globox1997 <mitschke11@web.de>
2023-03-31 08:45:02 +0200

    removed client bound

--- src/main/java/net/nameplate/Nameplate.java
+++ src/main/java/net/nameplate/Nameplate.java
@@ -3,21 +3,18 @@ package net.nameplate;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import net.nameplate.config.NameplateConfig;
 import net.nameplate.network.MobLevelPacket;
 
-@Environment(EnvType.CLIENT)
 public class Nameplate implements ClientModInitializer {
 
     public static NameplateConfig CONFIG = new NameplateConfig();
 
     public static final boolean isRpgDifficultyLoaded = FabricLoader.getInstance().isModLoaded("rpgdifficulty");
 
-    public static Identifier MOB_LEVEL_INFO = new Identifier("nameplate", "mob_level_info");
+    public static final Identifier MOB_LEVEL_INFO = new Identifier("nameplate", "mob_level_info");
 
     @Override
     public void onInitializeClient() {
